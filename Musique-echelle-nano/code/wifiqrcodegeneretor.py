65310fbc55ece87e253cb6f2d52050ed8260a3e0
leohuet <leho-huet@hotmail.fr>
2023-09-20 16:33:43 +0200

    Clean2

--- Musique-echelle-nano/code/wifiqrcodegeneretor.py
+++
@@ -1,7 +0,0 @@
-import wifi_qrcode_generator.generator
-
-qr_code = wifi_qrcode_generator.generator.wifi_qrcode(
-    ssid='PISSENLIT', hidden=False, authentication_type='WPA', password='sunnyoctopus384'
-)
-qr_code.print_ascii()
-qr_code.make_image().save('qr.png')
